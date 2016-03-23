870f24df9f2f5bb6f84ca29bd70115b9d0b40dc6
Nam (Nick) N. HUYNH <nick@enclave.vn>
2016-03-23 13:50:14 +0700

    Playing audio file in the background example.

--- PlayingAudioBackground/ViewController.swift
+++ PlayingAudioBackground/ViewController.swift
@@ -7,12 +7,72 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var audioPlayer: AVAudioPlayer?
+    
+    func handleInterruption(notification: NSNotification) {
+        
+        let interrupTypeAsObject = notification.userInfo![AVAudioSessionInterruptionTypeKey] as NSNumber
+        let interrupType = AVAudioSessionInterruptionType(rawValue: interrupTypeAsObject.unsignedLongValue)
+        if let type = interrupType {
+            
+            if type == AVAudioSessionInterruptionType.Ended {
+                
+                // Ended - Resum if needed
+                
+            }
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let dispatchQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        dispatch_async(dispatchQueue, { () -> Void in
+            
+            var audioSessionError: NSError?
+            let audioSession = AVAudioSession.sharedInstance()
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleInterruption:", name: AVAudioSessionInterruptionNotification, object: nil)
+            audioSession.setActive(true, error: nil)
+            
+            if audioSession.setCategory(AVAudioSessionCategoryPlayback, error: &audioSessionError) {
+                
+                println("Successfully set the audio session!")
+                
+            } else {
+                
+                println("Could not set audio session!")
+                
+            }
+            
+            let filePath = NSBundle.mainBundle().pathForResource("MySong", ofType: "mp3")
+            let fileData = NSData(contentsOfFile: filePath!, options: NSDataReadingOptions.DataReadingMappedIfSafe, error: nil)
+            var error: NSError?
+            self.audioPlayer = AVAudioPlayer(data: fileData, error: &error)
+            if let theAudioPlayer = self.audioPlayer {
+                
+                theAudioPlayer.delegate = self
+                if theAudioPlayer.prepareToPlay() && theAudioPlayer.play() {
+                    
+                    println("Successfully started playing!")
+                    
+                } else {
+                    
+                    println("Failed to play!")
+                    
+                }
+                
+            } else {
+                
+                // Failed
+                
+            }
+            
+        })
     }
 
     override func didReceiveMemoryWarning() {
